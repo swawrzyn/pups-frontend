e4773b15bda0726c73dc50fd4e4dcf5729fe73e2
Stefan Wawrzyn <swawrzyn@gmail.com>
2018-11-20 11:16:05 +0800

    finished index page, added show page and added link to show route

---
+++ pups/show/show.js
@@ -0,0 +1,74 @@
+// pups/show/show.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    const page = this
+    wx.request({
+      url: `http://localhost:3000/api/v1/restaurants/${options.id}`,
+      method: 'GET',
+      success(res) {
+        console.log(res)
+        page.setData({ pup: res.data });
+      }
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
